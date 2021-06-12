f92e518087d377f5188e7b84c9728af901e34423
Pallavi Maity <dmaity897@gmail.com>
2021-06-13 01:17:57 +0530

    Added delete functionality

--- script.js
+++ script.js
@@ -30,7 +30,9 @@ submit.addEventListener('click', ()=>{
 	}
 	addBooktoLibrary(bookName, authorName, pages, readVal);
 	bookForm.style.display = 'none';
+
 	showBooks();
+	location.reload();
 });
 
 function Book(title, author, noOfPages, read){
@@ -58,10 +60,16 @@ function showBooks(){
 	for(let i=0;i<books.length;i++){
 		let bookDiv = document.createElement('div');
 		bookDiv.classList.add('book');
+		bookDiv.setAttribute('id', i);
 		let t = document.createElement('h2');
 		let a = document.createElement('h3');
 		let n = document.createElement('h4');
 		let r = document.createElement('h4');
+		let b = document.createElement('button');
+		b.textContent = "Delete";
+		//b.setAttribute('id', 'deleteButton');
+		b.classList.add("deleteButton");
+		b.setAttribute('id',i);
 		t.textContent = books[i].title;
 		a.textContent = books[i].author;
 		n.textContent = books[i].noOfPages;
@@ -70,11 +78,32 @@ function showBooks(){
 		bookDiv.appendChild(a);
 		bookDiv.appendChild(n);
 		bookDiv.appendChild(r);
+		bookDiv.appendChild(b);
 		showAllBooks.appendChild(bookDiv);
-
 	}
-	
+	addDeleteToButton();
 }
 
 
+function deleteBook(e){
+	let bookId = e.target.id;
+	console.log(e.target.id);
+	alert("works fine");
+	let library1 = JSON.parse(localStorage.getItem('library'));
+	//library1 = library1.map(x => JSON.parse(x));
+	//console.log(library1[bookId]);
+	library1.splice(bookId, 1);
+	//console.log(library1);
+	//console.log(JSON.stringify(library1));
+	//library1.push(JSON.stringify(book));
+	localStorage.setItem('library', JSON.stringify(library1));
+	location.reload();
+}
+
+function addDeleteToButton(){
+	let deleteButton = document.getElementsByClassName("deleteButton");
+	let buttons = Array.from(deleteButton);
+	//let c = [...deleteButton];
+	buttons.map(x => x.addEventListener('click', deleteBook));
+}
 
