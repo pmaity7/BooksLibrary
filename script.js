dc09a3ac832bb3209396a4d23dfd13161f6f4988
Pallavi Maity <dmaity897@gmail.com>
2021-06-13 13:21:11 +0530

    Added change read status functionality

--- script.js
+++ script.js
@@ -40,14 +40,26 @@ function Book(title, author, noOfPages, read){
 	this.author = author;
 	this.noOfPages = noOfPages;
 	this.read = read;
-	return {title, author, noOfPages, read};
+	//return {title, author, noOfPages, read};
 }
+
+Book.prototype.change = function(){
+	if(this.read == "Read"){
+		this.read = "Not Read";
+	}else if(this.read == "Not Read"){
+		this.read = "Read";
+	}
+}
+
 
 function addBooktoLibrary(title, author, noOfPages, read){
 	let book = new Book(title, author, noOfPages, read);
-	library.push(JSON.stringify(book));
+	//library.push(JSON.stringify(book));
+	library.push(JSON.stringify({title, author, noOfPages, read}));
 	localStorage.setItem('library',JSON.stringify(library));
 }
+
+
 
 function getBooks(){
 	let libArray = JSON.parse(localStorage.getItem('library'));
@@ -66,10 +78,15 @@ function showBooks(){
 		let n = document.createElement('h4');
 		let r = document.createElement('h4');
 		let b = document.createElement('button');
+		let c = document.createElement('button');
+		r.setAttribute('id', 'readStatus'+i);
 		b.textContent = "Delete";
+		c.textContent = "Change Read Status"
 		//b.setAttribute('id', 'deleteButton');
 		b.classList.add("deleteButton");
 		b.setAttribute('id',i);
+		c.classList.add("changeButton");
+		c.setAttribute('id',i);
 		t.textContent = books[i].title;
 		a.textContent = books[i].author;
 		n.textContent = books[i].noOfPages;
@@ -79,9 +96,11 @@ function showBooks(){
 		bookDiv.appendChild(n);
 		bookDiv.appendChild(r);
 		bookDiv.appendChild(b);
+		bookDiv.appendChild(c);
 		showAllBooks.appendChild(bookDiv);
 	}
 	addDeleteToButton();
+	addChangeToButton();
 }
 
 
@@ -105,5 +124,36 @@ function addDeleteToButton(){
 	let buttons = Array.from(deleteButton);
 	//let c = [...deleteButton];
 	buttons.map(x => x.addEventListener('click', deleteBook));
+}
+
+
+
+function addChangeToButton(){
+	let changeButton = document.getElementsByClassName("changeButton");
+	let buttons = Array.from(changeButton);
+	//let c = [...deleteButton];
+	buttons.map(x => x.addEventListener('click', changeReadStatus));
+}
+
+
+function changeReadStatus(e){
+	let library1 = JSON.parse(localStorage.getItem('library'));
+	let bookId = e.target.id;
+	library1 = library1.map(x => JSON.parse(x));
+	console.log(library1);
+	let book = library1[bookId];
+	newBook = new Book(book.title, book.author, book.noOfPages, book.read);
+	newBook.change();
+	console.log(newBook);
+	let statusId = 'readStatus'+bookId;
+	let readStatus = document.getElementById(statusId);
+	readStatus.textContent = newBook.read;
+	//let books = JSON.parse(library1);
+	//console.log(books);
+	library1[bookId].read = newBook.read;
+	library1 = library1.map(x => JSON.stringify(x));
+	console.log(JSON.stringify(library1));
+	localStorage.setItem('library', JSON.stringify(library1));
+	location.reload();
 }
 
